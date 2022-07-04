ea4096a66a7e1a43a9cb6084c1495d915b559dec
Sushant <sushantverma9648@gmail.com>
2022-07-04 10:05:56 +0530

    Authentication

--- metrowebapp/views.py
+++ metrowebapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from metrowebapp.models import contactForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 # Create your views here.
 
 def home(request):
@@ -44,7 +47,36 @@ def saveContact(request):
         return render(request, "contact.html")
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        name = request.POST['name']
+        email = request.POST['email']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.name = name
+
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created.")
+
+        return redirect('signin')
     return render(request, "signup.html")
 def signin(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+
+        user = authenticate(username=username, password=pass1)
+
+        if user is not None:
+            login(request, user)
+            name = user.username
+            return render(request, "signedin.html", {'name':name})
+        
+        else:
+            messages.error(request, "Invalid User")
+            return redirect('home')
     return render(request, "signin.html")
 
